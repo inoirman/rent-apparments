2f5bb63bb348d1ce3a6fbf57ee1b77b1a8ac856a
Ignaty Kashnitsky <noir.ik@gmail.com>
2021-01-10 13:34:21 +0200

    add materialize css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'materialize-css'; // It installs the JS asset only
+import 'materialize-css/dist/css/materialize.min.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
